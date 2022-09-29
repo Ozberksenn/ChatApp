962788d3477e91f85ff6dc7de951347933d0d08c
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-29 22:59:39 +0300

    Chat page
    The users who sent the message are displayed on the chat page

--- src/screens/Main/Chats/Chats.js
+++ src/screens/Main/Chats/Chats.js
@@ -4,21 +4,76 @@ import styles from "./Chats.style";
 import ChatCard from "../../../components/ChatCard/ChatCard";
 import Stories from "../../../components/Stories/Stories";
 import { useSelector } from "react-redux";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { firestore } from "../../../../config";
 
 const Chats = () => {
   const [storyData, setStoryData] = useState();
+  const [data, setData] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const userListId = [];
+  const userList = [];
+
   const { userInfo } = useSelector((state) => state.user);
 
   useEffect(() => {
+    if (messages.length > 0) {
+      messages.map((e) => {
+        if (e.receiver_id === userInfo.uid || e.sender_id === userInfo.uid) {
+          if (e.receiver_id === userInfo.uid) {
+            userListId.push(e.sender_id);
+          } else if (e.sender_id === userInfo.uid) {
+            userListId.push(e.receiver_id);
+          }
+        }
+      });
+      const uniqueUserList = Array.from(new Set(userListId));
+      setUsers(uniqueUserList);
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (data.length > 0) console.log("data", data);
+  }, [data]);
+
+  useEffect(() => {
+    if (users.length > 0) getUsers();
+  }, [users]);
+
+  const getUsers = async () => {
+    users.map(async (e) => {
+      const q = doc(firestore, "users", e);
+      await getDoc(q).then((res) => {
+        setData((prevData) => [...prevData, res.data()]);
+      });
+    });
+  };
+
+  useEffect(() => {
     getCollection();
+    getMessageUser();
   }, []);
 
   const getCollection = async () => {
     const response = collection(firestore, "users");
     await getDocs(response).then((e) => {
       setStoryData(e.docs.map((item) => item.data()));
+    });
+  };
+
+  const getMessageUser = async () => {
+    const response = collection(firestore, "messages");
+    await getDocs(response).then((e) => {
+      setMessages(e.docs.map((item) => item.data()));
     });
   };
 
@@ -36,6 +91,13 @@ const Chats = () => {
           />
         </View>
         <View style={styles.line}></View>
+        {data.length > 0 && (
+          <FlatList
+            style={{ height: "90%" }}
+            data={data}
+            renderItem={(item) => <ChatCard data={item} />}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
